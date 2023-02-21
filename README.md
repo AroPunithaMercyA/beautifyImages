1465a9a05b58c1612805ee68a7cbc7402a23b87e
AroPunithaMercyA <104750679+AroPunithaMercyA@users.noreply.github.com>
2023-02-21 05:59:05 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# what is this?
+
+ To beautify images with border radius, box shadows and margins.
+
+# installation
+
+`npm i beautifyImages --save`
+
+
+Then...
+
+```
+import beautifyImages from './node_modules/beautifyImages/index.js';
+beautifyImages({
+      shadow_type:'soft',
+      padding:false
+});
+```
+
+## options
+
+shadowing supports two options :
+
+* *shadow_type* - _hard | soft_ (Defaults to 'soft')
+* *padding* - _boolean_ (Defaults to false)
---
+++ index.js
@@ -0,0 +1,23 @@
+export default function beautifyImages(options)
+{
+    let images=document.querySelectorAll('.beautifyImages');
+    console.log(images);
+    console.log(options);
+    if(options.shadow_type==='hard')
+    {
+        options.shadow_type==='0px';
+    }
+    else
+    {
+        options.shadow_type='15px';
+    }
+    images.forEach(image => {
+        console.log(image);
+        image.style.boxShadow="10px 20px 30px black";
+        image.style.borderRadius="20px";
+        if(options.margin)
+        {
+            image.style.margin= "50px";
+        } 
+      });
+}
